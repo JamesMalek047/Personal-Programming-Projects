9a0324b063ea9e5d207e370a72793a89cdc1b13e
James Malek <jammalek@hotmail.com>
2024-09-19 17:44:16 -0400

    My first commit

---
+++ OneDrive - University of Ottawa/Documents/buddyCarry/SignUp.java
@@ -0,0 +1,8 @@
+import java.util.Scanner; 
+public class SignUp {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("iBuddyCarry, your iBuddy for wherever you go!");
+        System.out.println("////////////////////")
+    }
+}
